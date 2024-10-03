c773eb8ef37c974ac3b397db242d1958a9e444b8
PachyuChepe <pachyuchepe@gmail.com>
2024-10-04 06:53:33 +0900

    [Feat] dev: 날짜 계산을 위한 addDays 유틸리티 함수 추가
    - 주어진 날짜(Date)에 특정 일수를 더해 새로운 날짜 반환
    - 원본 날짜 객체를 수정하지 않고 복사본을 생성해 처리
    - setDate와 getDate를 사용해 일(day) 계산 수행

---
+++ src/util/date-utils.ts
@@ -0,0 +1,6 @@
+// src/util/date-utils.ts
+export function addDays(date: Date, days: number): Date {
+  const newDate = new Date(date);
+  newDate.setDate(date.getDate() + days);
+  return newDate;
+}

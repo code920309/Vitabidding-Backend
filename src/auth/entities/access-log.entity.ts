341deae3b19a83b38c7e7c365b4dcb5f8db76b92
PachyuChepe <pachyuchepe@gmail.com>
2024-10-12 02:34:59 +0900

    [Feat] dev: AccessLog 및 AccessToken 엔티티 추가
    - AccessLog 엔티티로 사용자 활동 로그 저장 (User-Agent, IP, endpoint, accessedAt)
    - AccessToken 엔티티로 JWT 토큰 관리 (jti, token, 만료 시간, 취소 여부)
    - User 엔티티와의 Many-to-One 관계 설정
    - 사용자 삭제 시 관련 액세스 로그 자동 삭제 (onDelete: 'CASCADE')

---
+++ src/auth/entities/access-log.entity.ts
@@ -0,0 +1,25 @@
+// src/auth/entities/access-log.entity.ts
+import { Column, Entity, ManyToOne, Relation } from 'typeorm';
+import { BaseEntity } from '../../common/entity';
+import { User } from './user.entity';
+
+@Entity()
+export class AccessLog extends BaseEntity {
+  @Column({ type: 'varchar', length: 512, nullable: true })
+  ua: string;
+
+  @Column()
+  endpoint: string;
+
+  @Column()
+  ip: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  accessedAt: Date;
+
+  @ManyToOne(() => User, (user) => user.accessLogs, {
+    nullable: true,
+    onDelete: 'CASCADE',
+  })
+  user?: Relation<User>;
+}

d18d79cdbeef92c832de5113c549279f92e5e773
PachyuChepe <pachyuchepe@gmail.com>
2024-10-12 03:11:29 +0900

    [Feat] dev: 인증 관련 레포지토리 추가
    - UserRepository: 사용자 생성 및 이메일로 사용자 조회
    - AccessLogRepository: 사용자 활동 로그 저장
    - AccessTokenRepository: 액세스 토큰 저장 및 JWT ID로 조회
    - RefreshTokenRepository: 리프레시 토큰 저장 및 관리
    - TokenBlacklistRepository: 블랙리스트 토큰 저장 및 만료된 토큰 제거

---
+++ src/auth/repositories/index.ts
@@ -0,0 +1,6 @@
+// src/auth/repositories/index.ts
+export * from './user.repository';
+export * from './access-log.repository';
+export * from './access-token.repository';
+export * from './refresh-token.repository';
+export * from './token-blacklist.repository';
